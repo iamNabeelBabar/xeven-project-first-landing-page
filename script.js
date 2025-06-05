00e33887aaa8381fc686ed429b7b012d545e44f0
Hongkiat_clone <iamnabeel528@gmail.com>
2025-06-05 21:50:22 +0500

    Xeven first landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+$(document).ready(function(){
+  $('.cont9-box').slick({
+    arrows: true,        // Show next/prev arrows
+    dots: true,          // Optional: show navigation dots
+    slidesToShow: 1,     // Show one testimonial at a time
+    slidesToScroll: 1
+  });
+});
+</script>
